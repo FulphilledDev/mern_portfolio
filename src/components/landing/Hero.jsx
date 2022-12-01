146591486e509b9e470ec4771c2a00a01fdd59b2
Philip Simpson <dev.phits@gmail.com>
2022-12-01 14:41:51 -0800

    Update Hero: Live Demo Styles

--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -1,6 +1,20 @@
+import { Fragment, useState } from 'react';
 import { PopupButton } from "react-calendly";
+import { Dialog, Transition } from '@headlessui/react'
+import LiveDemo from './LiveDemo';
 
 const Hero = () => {
+
+  const [liveDemo, setLiveDemo] = useState(false)
+
+  const showDemo = () => {
+    if (!liveDemo) {
+      setLiveDemo(true)
+    } else {
+      setLiveDemo(false)
+    }
+  }
+
 
 return (
 <main>
@@ -36,12 +50,68 @@ return (
                         text="Click here to schedule!"
                         className="flex items-center justify-center mx-auto rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8 w-full"
                       />
-                      <a
-                        href="#!"
-                        className="flex items-center justify-center rounded-md border border-transparent bg-gradient-to-r from-blue-800 to-teal-700 bg-opacity-60 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-opacity-70 hover:from-blue-700 hover:to-teal-600 sm:px-8"
+                      <button
+                        onClick={showDemo}
+                        className="flex items-center justify-center mx-auto rounded-md border border-transparent bg-gradient-to-r from-blue-800 to-teal-700 bg-opacity-60 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-opacity-70 hover:from-blue-700 hover:to-teal-600 sm:px-8 w-full"
                       >
                         Live demo
-                      </a>
+                      </button>
+                      {liveDemo ? (
+                        <Transition.Root 
+                          show={liveDemo} 
+                          as={Fragment}
+                        >
+                          <Dialog 
+                            as="div" 
+                            className="relative z-10" onClose={setLiveDemo}>
+                            <Transition.Child
+                              as={Fragment}
+                              enter="ease-out duration-300"
+                              enterFrom="opacity-0"
+                              enterTo="opacity-100"
+                              leave="ease-in duration-200"
+                              leaveFrom="opacity-100"
+                              leaveTo="opacity-0"
+                            >
+                              <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                            </Transition.Child>
+
+                            <div className="fixed inset-0 z-10 overflow-y-auto">
+                              <div className="flex min-h-full items-end items-center justify-center p-4 text-center sm:items-center sm:p-0">
+                                <Transition.Child
+                                  as={Fragment}
+                                  enter="ease-out duration-300"
+                                  enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                  enterTo="opacity-100 translate-y-0 sm:scale-100"
+                                  leave="ease-in duration-200"
+                                  leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                                  leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                >
+                                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-2xl sm:p-6">
+                                    <div>
+                                      <div className="mt-3 text-center sm:mt-5">
+                                        <div className="mt-2">
+                                          <LiveDemo 
+                                            />
+                                        </div>
+                                      </div>
+                                    </div>
+                                    <div className="mt-5 sm:mt-6">
+                                      <button
+                                        type="button"
+                                        className="inline-flex w-full justify-center rounded-md border border-transparent bg-gradient-to-r from-blue-800 to-teal-800 px-4 py-2 text-base font-medium text-white shadow-sm hover:from-blue-700 hover:to-teal-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:text-sm"
+                                        onClick={() => setLiveDemo(false)}
+                                      >
+                                        Go back
+                                      </button>
+                                    </div>
+                                  </Dialog.Panel>
+                                </Transition.Child>
+                              </div>
+                            </div>
+                          </Dialog>
+                        </Transition.Root>
+                                          ) : null}
                       
                       {/* Insert modal of A3 website loom here */}
                     </div>
---
+++ src/components/landing/LiveDemo.jsx
@@ -0,0 +1,16 @@
+import A3Demo from '../../assets/images/loom_a3ecommerce.gif'
+
+const LiveDemo = () => {
+  return (
+    <div className='grid grid-cols-2 flex items-center'>
+        <div className='col-span-2 mx-auto'>
+            <a href="https://github.com/phitdev/a3-ecommerce-test2">
+                <img src={A3Demo} alt="A3 ECommerce"/>
+            </a>
+        </div>
+    </div>
+    
+  )
+}
+
+export default LiveDemo
